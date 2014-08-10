3d34656c3ea7e8028290e58f7deba82405714a92
hexrays <jeffdphoto@gmail.com>
2014-08-10 19:22:52 -0400

    Organized js and removed console.logs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,126 +1,118 @@
-// (function(){
+(function(){
+    'use strict';
 
-// })();
+    var app = app || {};
 
-var app = app || {};
+    app.list = {
+        el        : document.querySelector('.km-ul'),
+        $lastItem : null,
+        els       : [],
+        used      : [],
+        free      : [
+            'Design',
+            'Art Direction',
+            'Ui & Ux',
+            'Print',
+            'New York',
+            'Kathryn',
+            'Smart <span class="km-st">Ass</span> Design',
+            'Katie',
+            'Brooklyn',
+            'Interaction Design',
+            'User Experience',
+            'Whiskey Love',
+            'Cat Love',
+            'project manager',
+            'Team Leader',
+            'Typography',
+            'Branding',
+            'Start-ups',
+            'boutique agencies',
+            'Apps',
+            'Websites',
+            'Responsive',
+            'Logos',
+            'Signage',
+            'Packaging',
+            'iOS',
+            'Android',
+            'Freelance',
+            'Fulltime',
+            'Mentor',
+            'NYC',
+            'GD',
+            'Mangano',
+            'A lot of fun',
+            'Great work',
+            'Good times'
+        ],
 
-app.list = {
-    el   : document.querySelector('.km-ul'),
-    $lastItem : null,
-    els  : [],
-    used : [],
-    free : [
-        'Design',
-        'Art Direction',
-        'Ui & Ux',
-        'Print',
-        'New York',
-        'Kathryn',
-        'Smart <span class="km-st">Ass</span> Design',
-        'Katie',
-        'Brooklyn',
-        'Interaction Design',
-        'User Experience',
-        'Whiskey Love',
-        'Cat Love',
-        'project manager',
-        'Team Leader',
-        'Typography',
-        'Branding',
-        'Start-ups',
-        'boutique agencies',
-        'Apps',
-        'Websites',
-        'Responsive',
-        'Logos',
-        'Signage',
-        'Packaging',
-        'iOS',
-        'Android',
-        'Freelance',
-        'Fulltime',
-        'Mentor',
-        'NYC',
-        'GD',
-        'Mangano',
-        'A lot of fun',
-        'Great work',
-        'Good times'
-    ],
+        init: function() {
+            this.makeList();
+        },
 
-    init: function() {
-        this.makeList();
-        console.log('whu?');
-    },
+        makeList : function() {
+            var frag       = document.createDocumentFragment(),
+                totalItems = this.free.length,
+                itemNumber, slogan, listItem;
 
-    makeList : function(free, used) {
-        var frag       = document.createDocumentFragment(),
-            totalItems = this.free.length,
-            itemNumber, slogan, listItem;
+            while(totalItems--) {
+                itemNumber = Math.floor(Math.random() * totalItems);
+                slogan     = this.free[itemNumber];
 
-        while(totalItems--) {
-            itemNumber = Math.floor(Math.random() * totalItems);
-            slogan     = this.free[itemNumber];
+                listItem   = this.createLi(slogan);
 
-            listItem   = this.createLi(slogan);
+                this.free.splice(itemNumber, 1);
+                this.used.push(slogan);
 
-            this.free.splice(itemNumber, 1);
-            this.used.push(slogan);
-
-            frag.appendChild(listItem);
-        }
-        this.el.appendChild(frag);
-        this.resetFree();
-        this.setLast();
-    },
-
-    setLast: function(){
-        var self = this;
-        this.$lastItem = $('.km-slogan:nth-last-child(6)');
-
-        this.$lastItem.waypoint(function(direction){
-            console.log('here ' + direction);
-            // self.$lastItem = null;
-            if(direction === 'down') {
-                self.destroyWaypoint();
-                self.makeList();
+                frag.appendChild(listItem);
             }
+            this.el.appendChild(frag);
+            this.resetFree();
+            this.setLast();
+        },
 
-        },{
-            offset: '100%'
-        });
+        setLast: function(){
+            var self = this;
+            this.$lastItem = $('.km-slogan:nth-last-child(6)');
 
-    },
+            this.$lastItem.waypoint(function(direction){
+                // self.$lastItem = null;
+                if(direction === 'down') {
+                    self.destroyWaypoint();
+                    self.makeList();
+                }
 
-    destroyWaypoint: function(){
-        this.$lastItem.waypoint('destroy');
-    },
+            },{
+                offset: '100%'
+            });
 
-    createLi: function(words){
-        var li = document.createElement('li'),
-            copy = words + '<span class="km-slash"> / </span>';
+        },
 
-        li.setAttribute('class', 'km-slogan');
-        li.innerHTML = copy;
+        destroyWaypoint: function(){
+            this.$lastItem.waypoint('destroy');
+        },
 
-        return li;
-    },
+        createLi: function(words){
+            var li = document.createElement('li'),
+                copy = words + '<span class="km-slash"> / </span>';
 
-    // resetFree: function() {
-    //     // var totalItems = this.used.length;
-    //     if(this.free.length === 0 ){
-    //         this.makeList(this.used, this.free);
-    //     }
+            li.setAttribute('class', 'km-slogan');
+            li.innerHTML = copy;
 
-    // }
-    resetFree: function(){
-        this.free = this.used.slice(0);
-        this.used = [];
-    }
-};
+            return li;
+        },
+
+        resetFree: function(){
+            this.free = this.used.slice(0);
+            this.used = [];
+        }
+    };
 
 
-$(document).ready(function(){
-    app.list.init();
-    $('li').hyphenate('en-us');
-});
+    $(document).ready(function(){
+        app.list.init();
+        $('li').hyphenate('en-us');
+    });
+
+})();
